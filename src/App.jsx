d097b9fcd61fb9dadd36888ad4e39e154306f793
Ayush Gautam <ayushgau194@gmail.com>
2025-07-02 20:36:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
